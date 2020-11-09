58369c98bf24f31603c89b5681d55060c8634a9e
geekbutchic <71285487+geekbutchic@users.noreply.github.com>
2020-11-09 15:00:02 -0500

    added 3 second function

--- main.js
+++ main.js
@@ -1,2 +1,21 @@
 // version 0:
-// console.log('\u0007');
+console.log('\u0007');
+
+
+
+const babyTimer = function() {
+    console.log('3 seconds has passed');
+    console.log(`\u0007`);
+}
+
+setTimeout(babyTimer, 3000)
+
+
+
+
+
+
+
+
+
+
